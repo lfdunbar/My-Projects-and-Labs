0de2563eca565c9654fb4e20e0d89b161a30db7c
lfdunbar <89544700+lfdunbar@users.noreply.github.com>
2021-08-25 17:42:23 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+/*Hello.java
+Laila Dunbar
+08/25/2021
+CMSC 255*/
+public class Hello { 
+    public static void main (String[] args) {
+System.out.println("<Laila Dunbar>");
+System.out.println("<Hello.java>");
+System.out.println("<Project 1>");
+System.out.println("<08/25/2021>");
+System.out.println("<CMSC 255>");
+System.out.println();
+    }
+}
